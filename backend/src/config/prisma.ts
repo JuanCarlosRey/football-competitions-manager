b95749ba54964d2309b3e227e00d7c6c51288c7f
JuanCarlosRey <alu0101410869@ull.edu.es>
2026-08-25 16:03:34 +0100

    refactor: rename frontend and backend

--- backend/src/config/prisma.ts
+++ backend/src/config/prisma.ts
@@ -1,6 +1,6 @@
 import { PrismaClient } from '@prisma/client';
 import { PrismaPg } from '@prisma/adapter-pg';
-import pg from 'pg';
+import pg from '../../node_modules/@types/pg/index.js';
 
 if (!process.env.DATABASE_URL) {
     throw new Error('DATABASE_URL is not defined in environment variables');
